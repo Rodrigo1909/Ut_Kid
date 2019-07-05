e8a5b80fbe95e2cd6f386683f7f74a6f56dbc46d
Rodrigo Montero Orozco <rodrigo.montero1909@gmail.com>
2019-07-05 10:48:06 -0500

    Login

---
+++ Ut_Kid/Ut_Kid/Login.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Ut_Kid.Login" %>
+
+<!DOCTYPE html>
+
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Ingresa tu cuenta</title>
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <script src="Scripts/jquery-1.10.2.min.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
+    <link href="Content/EstiloFondo.css" rel="stylesheet" />
+    <style type="text/css">
+        .offset-md-3 {
+            top: -50px;
+            left: 350px;
+            width: 445px;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container">
+        <div class="login-form col-md-5 offset-md-3">
+            <h1 class="title">Acceso al Sistema</h1>
+            <form id="form1" runat="server">
+
+                <div class="form-group">
+                    <asp:Label ID="Label1" runat="server" Text="Usuario" BackColor="White"></asp:Label>
+                    <asp:TextBox ID="txtUser" runat="server" CssClass="form-control input-sm" placeholder="Ingresa tu usuario"></asp:TextBox>
+                </div>
+
+                <div class="form-group ">
+                    <asp:Label ID="Label2" runat="server" Text="Contraseña" BackColor="White"></asp:Label>
+                    <asp:TextBox ID="txtPass" runat="server" CssClass="form-control input-sm" placeholder="Ingresa la contraseña: " TextMode="Password"></asp:TextBox>
+                </div>
+                      
+                <br />
+
+                <div class="form-group">
+                     <asp:Button ID="btnLogin" runat="server" CssClass="btn btn-block btn-sm btn-primary" Text="Ingresar" OnClick="btnLogin_Click"  />
+                </div>
+
+                <asp:Label ID="Label3" runat="server" Text="¿Eres nuevo? Crea tu cuenta" BackColor="White"></asp:Label>
+                <div class="form_group">
+                      <asp:Button ID="btnRegistro" runat="server" CssClass="btn  btn-block btn-sm btn-success" Text="Registrarse" OnClick="btnRegistro_Click" />
+                </div>
+                
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ Ut_Kid/Ut_Kid/Login.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Modelo;
+using Controller;
+
+namespace Ut_Kid
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            ValidarLogin(GetDatosVista());
+        }
+
+        protected TblUser GetDatosVista()
+        {
+            String user = txtUser.Text;
+            String pass = txtPass.Text;
+
+            TblUser usuario = new TblUser();
+            usuario.strusuario = user;
+            usuario.strpass = pass;
+            return usuario;
+
+        }
+
+        protected void ValidarLogin(TblUser usuario)
+        {
+            if (this.txtUser.Text.Equals("administrador") && this.txtPass.Text.Equals("1234"))
+            {
+                Session["user"] = txtUser.Text;
+                this.Response.Redirect("./Administrador/InicioAdmin.aspx", true);
+            }
+            ControllerAutenticacion ctrlAutenticacion = new ControllerAutenticacion();
+            TblUser UsuarioLoggeado = ctrlAutenticacion.ValidarLogin(usuario);
+            if (UsuarioLoggeado != null)
+            {
+                Session["user"] = usuario;
+                if (UsuarioLoggeado.strtipoUsuario == "PADRE")
+                {
+                    Response.Redirect("./Padre/InicioPadre.aspx", true);
+                }
+                else if (UsuarioLoggeado.strtipoUsuario == "PROFESOR")
+                {
+                    Response.Redirect("./Profesor/InicioProfesor.aspx", true);
+                }
+                else if (UsuarioLoggeado.strtipoUsuario == "NINO")
+                {
+                    Response.Redirect("./Nino/InicioNino.aspx", true);
+                }
+            }
+            else
+            {
+                //Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Usuario o contraseña incorrecta');</script>");
+                Response.Redirect("Login.aspx", true);
+                this.Response.Write("<script lenguage='JavaScript'>windows.alert('Fallo')</script>");
+            }
+        }
+
+        protected void btnRegistro_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/Registro/RegistroProfesor.aspx", true);
+        }
+    }
+}
